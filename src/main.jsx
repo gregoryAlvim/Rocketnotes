b6683a8141733fa95af7c00b11b1ab6824d72ebb
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-04 17:18:30 -0300

    Feat: some little changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
 import { ThemeProvider } from 'styled-components';
-import { Details } from './pages/Details';
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )

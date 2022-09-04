4fb598846cd4d9591c77a6756063b27b4648a501
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-03 22:15:07 -0300

    Feat: adding new components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import theme from './styles/theme';
+import GlobalStyles from './styles/global';
+
+import { ThemeProvider } from 'styled-components';
 import { Details } from './pages/Details';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 )

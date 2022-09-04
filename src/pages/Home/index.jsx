b44c7e78669f4ce905cb4acd726eb6d2c1f72343
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-04 17:14:53 -0300

    Chore: Creating components

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,33 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
+
+export function Home() {
+   return (
+      <Container>
+         <Brand>
+            <h1> Rocketnotes </h1>
+         </Brand>
+
+         <Header />
+
+         <Menu>
+            <li><ButtonText title="Todos" isActive /></li>
+            <li><ButtonText title="React" /></li>
+            <li><ButtonText title="Nodejs" /></li>
+         </Menu>
+
+         <Search>
+
+         </Search>
+
+         <Content>
+
+         </Content>
+
+         <NewNote>
+
+         </NewNote>
+      </Container>
+   );
+}

ec1a29f1750cf22dbe073ff82a722f43a3c5cfb7
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-07 16:16:09 -0300

    Feat: Home page Finalized

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,14 @@
+import { FiPlus, FiSearch } from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+
+import { Note } from '../../components/Note';
+import { Input } from '../../components/Input';
 import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 
-export function Home() {
+export function Home () {
    return (
       <Container>
          <Brand>
@@ -12,21 +18,30 @@ export function Home() {
          <Header />
 
          <Menu>
-            <li><ButtonText title="Todos" isActive /></li>
-            <li><ButtonText title="React" /></li>
-            <li><ButtonText title="Nodejs" /></li>
+            <li><ButtonText title = "Todos" isActive /></li>
+            <li><ButtonText title = "React" /></li>
+            <li><ButtonText title = "Nodejs" /></li>
          </Menu>
 
          <Search>
-
+            <Input placeholder = "Pesquisar nota pelo titulo" icon={FiSearch} />
          </Search>
 
          <Content>
-
+            <Section title = "Minhas notas">
+               <Note data = {{
+                  title: 'React', 
+                  tags: [
+                     {id: '1', name: 'React'},
+                     {id: '2', name: 'Rocketseat'}
+                  ]
+               }}/>
+            </Section>
          </Content>
 
          <NewNote>
-
+            <FiPlus />
+            Criar nota
          </NewNote>
       </Container>
    );

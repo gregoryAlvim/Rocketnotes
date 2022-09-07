3ac4999cfc954e4d973a0deef1fd6a2e17c4299c
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-07 16:24:14 -0300

    Chore: inicialing work with the app routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
 import { ThemeProvider } from 'styled-components';
-import { Home } from './pages/Home';
+import { New } from './pages/New';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )

7be1efeeaad203a9f16215132648edb99e032836
Grégory Alvim <gregori.jagua@gmail.com>
2022-09-10 11:47:59 -0300

    Feat: added the app routes and authentic routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
 import { ThemeProvider } from 'styled-components';
-import { New } from './pages/New';
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )

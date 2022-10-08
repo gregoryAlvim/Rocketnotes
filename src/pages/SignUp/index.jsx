bb7fae8204a2f28a6eb7b735ddd411a0c64875c6
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-08 17:19:58 -0300

    Feat: added user create a count rules and states

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { api } from '../../services/api';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -7,6 +10,32 @@ import { Button } from '../../components/Button';
 import { Container, Form, Background } from './styles';
 
 export function SignUp () {
+
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+   const navigate = useNavigate();
+
+   function handleSignUp() {
+      if(!name || !email || !password) {
+         return alert("Preencha todos os campos!");
+      }
+
+      api.post("/users", { name, email, password })
+      .then(() => {
+         alert("Usuário cadastrado com sucesso!");
+         navigate("/");
+      })
+      .catch(error => {
+         if (error.response) {
+            alert(error.response.data.message);
+         } else {
+            alert("Não foi possível realizar o cadastro!");
+         }
+      });
+   }
+
    return (
       <Container>
          <Background />
@@ -21,21 +50,27 @@ export function SignUp () {
                placeholder="Nome"
                type="text"
                icon={FiUser}
+               onChange={ event => setName(event.target.value)}
             />
 
             <Input 
                placeholder="E-mail"
                type="text"
                icon={FiMail}
+               onChange={ event => setEmail(event.target.value)}
             />
 
             <Input 
                placeholder="Senha"
                type="password"
                icon={FiLock}
+               onChange={ event => setPassword(event.target.value)}
             />
 
-            <Button title="Cadastrar" />
+            <Button 
+               title="Cadastrar"
+               onClick={handleSignUp}
+            />
 
             <Link to="/">
                Voltar para o login

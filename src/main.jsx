34e5c6cbae184cc830d0f5e75e1b1bd098ea7de9
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-08 17:16:21 -0300

    Feat: added AuthProvider for the user context

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,16 @@ import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
 import { Routes } from './routes';
 
+import { AuthProvider } from './hooks/auth';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )

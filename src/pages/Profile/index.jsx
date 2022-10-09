a719b9f6f726d1b9f6bddeed8e4979ecfc08d36d
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-09 18:06:08 -0300

    Feat: added news functionalities save state inputs data

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser, FiCamera } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 
-import { Container, Form, Avatar } from "./styles";
+import { useAuth } from '../../hooks/auth';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
+import { Container, Form, Avatar } from "./styles";
+
 export function Profile () {
+
+   const { user, updateProfile } = useAuth();
+
+   const [name, setName] = useState(user.name);
+   const [email, setEmail] = useState(user.email);
+   const [passwordOld, setPasswordOld] = useState();
+   const [passwordNew, setPasswordNew] = useState();
+
+   async function handleUpdate() {
+
+      const user = {
+         name,
+         email,
+         password: passwordNew,
+         old_password: passwordOld,
+      };
+
+      await updateProfile({ user });
+   }
+
    return (
       <Container>
          <header>
@@ -36,27 +59,36 @@ export function Profile () {
                placeholder="Name"
                type="text"
                icon={FiUser}
+               value={name}
+               onChange={ event => setName(event.target.value) }
             />
 
             <Input
                placeholder="E-mail"
                type="text"
                icon={FiMail}
+               value={email}
+               onChange={ event => setEmail(event.target.value) }
             />
 
             <Input
                placeholder="Senha atual"
                type="password"
                icon={FiLock}
+               onChange={ event => setPasswordOld(event.target.value) }
             />
 
             <Input
                placeholder="Nova senha"
                type="password"
                icon={FiLock}
+               onChange={ event => setPasswordNew(event.target.value) }
             />
 
-            <Button title="Salvar" />
+            <Button 
+               title="Salvar"
+               onClick={handleUpdate}
+            />
             
          </Form>
       </Container>

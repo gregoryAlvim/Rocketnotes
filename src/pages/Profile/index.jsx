dd54f6f9969d2a62084797a02fa56b0116194403
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-10 20:42:14 -0300

    Feat: added user avatar from api and added upload avatar to bd

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,9 +3,12 @@ import { FiArrowLeft, FiMail, FiLock, FiUser, FiCamera } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
+import { api } from '../../services/api';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
 import { Container, Form, Avatar } from "./styles";
 
@@ -18,6 +21,11 @@ export function Profile () {
    const [passwordOld, setPasswordOld] = useState();
    const [passwordNew, setPasswordNew] = useState();
 
+
+   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+   const [avatar, setAvatar] = useState(avatarUrl);
+   const [avatarFile, setAvatarFile] = useState(null);
+
    async function handleUpdate() {
 
       const user = {
@@ -27,7 +35,15 @@ export function Profile () {
          old_password: passwordOld,
       };
 
-      await updateProfile({ user });
+      await updateProfile({ user, avatarFile });
+   }
+
+   function handleChangeAvatar(event) {
+      const file = event.target.files[0];
+      setAvatarFile(file);
+
+      const imagePreview = URL.createObjectURL(file);
+      setAvatar(imagePreview);
    }
 
    return (
@@ -41,7 +57,7 @@ export function Profile () {
          <Form>
             <Avatar>
                <img 
-                  src="https://github.com/gregoryAlvim.png" 
+                  src={avatar}
                   alt="Foto do usuário" 
                />
 
@@ -51,6 +67,7 @@ export function Profile () {
                   <input 
                      id="avatar" 
                      type="file"
+                     onChange={ handleChangeAvatar }
                   />
                </label>
             </Avatar>

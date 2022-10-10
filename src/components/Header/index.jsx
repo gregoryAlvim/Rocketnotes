f77253453fbccfb8645fbe9dbe668b1aa3baf6a0
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-10 20:39:39 -0300

    Refactor: added user avatar from api and added upload avatar to bd

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,26 @@
 import { RiShutDownLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth';
 
+import { api } from '../../services/api';
+
 import { Container, Profile, Logout } from './styles';
 
 export function Header () {
 
-   const { signOut } = useAuth();
+   const { signOut, user } = useAuth();
+   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
    return (
       <Container>
          <Profile to="/profile">
             <img 
-               src="https://github.com/gregoryAlvim.png" 
-               alt="Foto do Usuário"
+               src={avatarUrl} 
+               alt={"Foto do usuário" + user.name}
             />
 
             <div>
                <span>Bem-vindo!</span>
-               <strong>Grégory Alvim</strong>
+               <strong>{user.name}</strong>
             </div>
          </Profile>
 

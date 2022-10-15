c008e75f43dfe83c0050c5c595bd4ff1ff71b9e0
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-15 16:19:37 -0300

    Refactor: changed navigate pages

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,24 @@
 import { RiShutDownLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 import { api } from '../../services/api';
 
 import { Container, Profile, Logout } from './styles';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
 export function Header () {
 
    const { signOut, user } = useAuth();
    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+   const navigate = useNavigate();
+
+   function handleSignOut() {
+      navigate("/");
+
+      signOut();
+   }
 
    return (
       <Container>
@@ -24,7 +34,7 @@ export function Header () {
             </div>
          </Profile>
 
-         <Logout onClick={signOut}>
+         <Logout onClick={handleSignOut}>
             <RiShutDownLine />
          </Logout>
       </Container>
